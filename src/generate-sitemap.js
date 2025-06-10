552dceda3327a612ca3ca8609ed408b0da384aef
Saqib Islam <saaqi.grw@gmail.com>
2025-06-10 07:55:19 +0500

    Update generate-sitemap.js

--- src/generate-sitemap.js
+++ src/generate-sitemap.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import { data } from '../src/app.js'; // adjust if needed
+import { data } from './app.js'; // adjust if needed
 
 const siteUrl = data.baseURL;
 
