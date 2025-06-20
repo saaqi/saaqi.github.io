6b5929f4638d5ec7795a496f7182042e1822fd15
Saqib Islam <saaqi.grw@gmail.com>
2025-06-20 18:15:36 +0500

    bug fixes.

--- src/components/ObfuscatedEmail.svelte
+++ src/components/ObfuscatedEmail.svelte
@@ -12,12 +12,10 @@
 	const encodedEmail = xorEmail(email, key);
 
 	// Deobfuscate on mount
-	let href = $state(encodedEmail);
+	let href = $state('/');
 	const decodedEmail = xorEmail(encodedEmail, key);
 	const extra = "?subject=Hi,%20Saqib%20Let's%20talk!";
-	const onclick = () => {
-		href = 'mailto:' + decodedEmail + extra;
-	};
+	const onclick = () => (href = 'mailto:' + decodedEmail + extra);
 </script>
 
 <a {onclick} {href} {...props}>
