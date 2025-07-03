d304f130e8063489cdc3af2163b4d4f0f4ccc635
Saqib Islam <saaqi.grw@gmail.com>
2025-07-03 14:28:52 +0500

    minor

--- src/components/ContactForm.svelte
+++ src/components/ContactForm.svelte
@@ -54,113 +54,111 @@
 </script>
 
 <form id="contact-email-form" class="contact-email-form">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6">
-				<label for="contact-form-name">
-					<svg class="icon user-icon small">
-						<use xlink:href={icons + '#user-icon'}></use>
-					</svg>
-					Name:
-				</label>
-				<input
-					bind:value={name}
-					type="text"
-					name="contact-form-name"
-					class="form-control {border.name}"
-					id="contact-form-name"
-					placeholder="Enter Your Full Name *"
-					required
-				/>
-			</div>
-			<div class="col-md-6 mt-3 mt-md-0">
-				<label for="contact-form-email">
-					<svg class="icon email-icon small">
-						<use xlink:href={icons + '#email-icon'}></use>
-					</svg>
-					Email Address:
-				</label>
-				<input
-					bind:value={email}
-					type="email"
-					class="form-control {border.email}"
-					name="contact-form-email"
-					id="contact-form-email"
-					placeholder="Enter Your Email *"
-					required
-				/>
-			</div>
-			{#if !emailValid && email}
-				<div class="col mt-3">
-					<div class="alert {alert} m-0" role="alert" transition:slide={{ duration: 100 }}>
-						Please enter a Valid E-Mail Address to continue.
-					</div>
-				</div>
-			{/if}
-		</div>
-		<div class="mt-3">
-			<label for="contact-form-subject">
-				<svg class="icon subject-icon small">
-					<use xlink:href={icons + '#subject-icon'}></use>
+	<div class="row">
+		<div class="col-md-6">
+			<label for="contact-form-name">
+				<svg class="icon user-icon small">
+					<use xlink:href={icons + '#user-icon'}></use>
 				</svg>
-				Subject:
+				Name:
 			</label>
 			<input
-				bind:value={subject}
+				bind:value={name}
 				type="text"
-				class="form-control"
-				name="contact-form-subject"
-				id="contact-form-subject"
-				placeholder="Enter The subject"
+				name="contact-form-name"
+				class="form-control {border.name}"
+				id="contact-form-name"
+				placeholder="Enter Your Full Name *"
+				required
 			/>
 		</div>
-		<div class="mt-3">
-			<label for="contact-form-message">
-				<svg class="icon message-icon small">
-					<use xlink:href={icons + '#message-icon'}></use>
-				</svg>
-				Message:
-			</label>
-			<textarea
-				bind:value={message}
-				class="form-control {border.message}"
-				name="contact-form-message"
-				id="contact-form-message"
-				rows="5"
-				placeholder="What is on your mind? *"
-				required
-			></textarea>
-		</div>
-		<div
-			class="submitContainer text-end text-md-start mt-4"
-			onmouseenter={showMainAlert}
-			onfocus={showMainAlert}
-			onmouseleave={hideMainAlert}
-			onblur={hideMainAlert}
-			role="presentation"
-		>
-			<button
-				{onclick}
-				type="submit"
-				class="btn {btn}"
-				disabled={disableSubmit}
-				aria-disabled={disableSubmit}
-			>
-				<svg class="icon email-icon">
+		<div class="col-md-6 mt-3 mt-md-0">
+			<label for="contact-form-email">
+				<svg class="icon email-icon small">
 					<use xlink:href={icons + '#email-icon'}></use>
 				</svg>
-				Send Message
-			</button>
-			{#if mainAlert}
-				<div
-					class="alert {alert} text-center text-md-start mt-3"
-					role="alert"
-					transition:slide={{ duration: 100 }}
-				>
-					Please complete the form before sending your message.
-				</div>
-			{/if}
+				Email Address:
+			</label>
+			<input
+				bind:value={email}
+				type="email"
+				class="form-control {border.email}"
+				name="contact-form-email"
+				id="contact-form-email"
+				placeholder="Enter Your Email *"
+				required
+			/>
 		</div>
+		{#if !emailValid && email}
+			<div class="col mt-3">
+				<div class="alert {alert} m-0" role="alert" transition:slide={{ duration: 100 }}>
+					Please enter a Valid E-Mail Address to continue.
+				</div>
+			</div>
+		{/if}
+	</div>
+	<div class="mt-3">
+		<label for="contact-form-subject">
+			<svg class="icon subject-icon small">
+				<use xlink:href={icons + '#subject-icon'}></use>
+			</svg>
+			Subject:
+		</label>
+		<input
+			bind:value={subject}
+			type="text"
+			class="form-control"
+			name="contact-form-subject"
+			id="contact-form-subject"
+			placeholder="Enter The subject"
+		/>
+	</div>
+	<div class="mt-3">
+		<label for="contact-form-message">
+			<svg class="icon message-icon small">
+				<use xlink:href={icons + '#message-icon'}></use>
+			</svg>
+			Message:
+		</label>
+		<textarea
+			bind:value={message}
+			class="form-control {border.message}"
+			name="contact-form-message"
+			id="contact-form-message"
+			rows="5"
+			placeholder="What is on your mind? *"
+			required
+		></textarea>
+	</div>
+	<div
+		class="submitContainer text-end text-md-start mt-4"
+		onmouseenter={showMainAlert}
+		onfocus={showMainAlert}
+		onmouseleave={hideMainAlert}
+		onblur={hideMainAlert}
+		role="presentation"
+	>
+		<button
+			{onclick}
+			type="submit"
+			class="btn {btn}"
+			disabled={disableSubmit}
+			aria-disabled={disableSubmit}
+		>
+			<svg class="icon email-icon">
+				<use xlink:href={icons + '#email-icon'}></use>
+			</svg>
+			Send Message
+		</button>
+		{#if mainAlert}
+			<div
+				class="alert {alert} text-center text-md-start mt-3"
+				role="alert"
+				transition:slide={{ duration: 100 }}
+			>
+				Please complete the form before sending your message.
+			</div>
+		{/if}
 	</div>
 </form>
 
